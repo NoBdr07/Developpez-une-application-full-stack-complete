2640fd1eb1e1df72ce4e1b4945693e435bc49e95
Noémie <baudry.ddf@gmail.com>
2024-06-18 22:34:17 +0200

    register et login submit ok

--- front/src/app/pages/auth/register/register.component.ts
+++ front/src/app/pages/auth/register/register.component.ts
@@ -3,46 +3,43 @@ import { AuthService } from '../services/auth.service';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { SessionService } from 'src/app/services/session.service';
-import { AuthSuccess } from '../interfaces/authSucess.interface';
 import { User } from 'src/app/interfaces/user.interface';
 import { RegisterRequest } from '../interfaces/registerRequest.interface';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent  {
+export class RegisterComponent {
   public onError = false;
 
-  constructor(private authService: AuthService,
+  constructor(
+    private authService: AuthService,
     private fb: FormBuilder,
     private router: Router,
     private sessionService: SessionService
-  ) { }
-
+  ) {}
 
   public form = this.fb.group({
     username: [''],
     email: [''],
-    password: ['', [Validators.required, Validators.pattern('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\\W_]).{8,}$')]]
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(
+          '^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\\W_]).{8,}$'
+        ),
+      ],
+    ],
   });
 
   public submit(): void {
     const registerRequest = this.form.value as RegisterRequest;
-    this.authService.register(registerRequest).subscribe(
-      (response: AuthSuccess) => {
-        localStorage.setItem('token', response.token);
-        console.log(localStorage.getItem);
-        this.authService.me().subscribe((user: User) => {
-          this.sessionService.logIn(user);
-          this.router.navigate(['/posts'])
-        });
-        this.router.navigate(['/posts'])
-      },
-      error => this.onError = true
-    );
-  
+    this.authService.register(registerRequest).subscribe(() => {
+      this.router.navigate(['/auth/login']);
+    });
+    (_error: any) => (this.onError = true);
   }
-
 }
